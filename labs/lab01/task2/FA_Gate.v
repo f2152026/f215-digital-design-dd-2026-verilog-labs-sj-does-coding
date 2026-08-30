963a177e3f65b940c9f5d68a4a16a020dd840ddf
SHUBHANKAR SINGH JAMWAL <f20250609@pilani.bits-pilani.ac.in>
2026-08-30 12:44:36 +0530

    Update FA_Gate.v

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -26,10 +26,10 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor (ps,  a,   b);
-  and (pc1, a,   b);
-  xor (sum, cin, ps);
-  and (pc2, cin, ps);
-  or  (cout, pc1, pc2);
+  xor #(2,3) (ps,  a,   b);
+  and #(4,5) (pc1, a,   b);
+  xor #(2,3) (sum, cin, ps);
+  and #(4,5) (pc2, cin, ps);
+  or  #(3,4) (cout, pc1, pc2);
 
 endmodule
